5f8b4ff62daca665702f66a56a70af2f0fb3ca26
Prerna2001 <53401621+Prerna2001@users.noreply.github.com>
2020-06-01 19:35:12 +0530

    Update que6.py

--- que6.py
+++ que6.py
@@ -1,3 +1,5 @@
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 s1=0
 s2=0
 for i in range (101):
@@ -5,3 +7,6 @@ for i in range (101):
     s2=s2+(i*i)
 s3=(s1*s1)-s2
 print(s3)
+
+
+#Output: 25164150
