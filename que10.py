df086e88dfb00ad4ff3674d55357c71581b4dee5
Prerna2001 <53401621+Prerna2001@users.noreply.github.com>
2020-06-06 23:39:57 +0530

    Update que10.py

--- que10.py
+++ que10.py
@@ -1,7 +1,7 @@
 #Find the sum of all the primes below two million.
 a = 2000000
 numbers = set(range(3, a + 1, 2))
-for number in range(3, int(a ** 0.5) + 1):
+for number in range(3, int(a ** 0.5) ,2):
     if number not in numbers:
         continue
         num = number
