1b721206524d368c5c21f2e2743eb2cd582a38e7
Prerna2001 <53401621+Prerna2001@users.noreply.github.com>
2020-06-07 14:39:38 +0530

    Update que6.py

--- que6.py
+++ que6.py
@@ -1,14 +1,18 @@
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 def sqsum(to):
-    return sum(i ** 2 for i in range(1, to))
-
+    if to==0:
+        return 0
+    else:
+        return to*(to+1)*(2*to+1)/6
 
 def sumsq(to):
-    return sum(i for i in range(1, to)) ** 2
+    if to==0:
+        return 0
+    else:
+        return (to*(to+1)/2)**2
 
 
-print(sumsq(101) - sqsum(101))
-
+print(sumsq(100)-sqsum(100))
 
 #Output: 25164150
