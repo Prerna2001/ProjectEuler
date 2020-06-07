eb589c83c8be8e9a3979b4d10f312a1d9910ab2b
Prerna2001 <53401621+Prerna2001@users.noreply.github.com>
2020-06-07 14:39:05 +0530

    Update que7.py

--- que7.py
+++ que7.py
@@ -2,20 +2,22 @@
 
 def isPrime(n):
     if n < 2: return "Neither prime, nor composite"
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(3, int(n**0.5)+1,2):
         if n % i == 0:
             return False
     return True
 
 
 def nthPrime(n):
-    numberOfPrimes = 0
+    numberOfPrimes = 1  # 2 is not in prime ( 1 st no. prime is 3
+
     prime = 1
 
     while numberOfPrimes < n:
-        prime += 1
+        prime += 2
         if isPrime(prime):
             numberOfPrimes += 1
+
     return prime
 
 print(nthPrime(10001))
